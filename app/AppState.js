4cea6ec8bb49a528a093f1d2304885f6f38a2f2a
Jake Overall <joverall22@gmail.com>
2023-11-08 00:16:54 -0700

    cleanup, adds better AppState

--- app/AppState.js
+++ app/AppState.js
@@ -1,30 +1,10 @@
-import { Value } from "./models/Value.js"
-import { EventEmitter } from "./utils/EventEmitter.js"
-import { isValidProp } from "./utils/IsValidProp.js"
-import { loadState } from "./utils/Store.js"
+import { EventEmitter } from './utils/EventEmitter.js'
+import { createObservableProxy } from './utils/ObservableProxy.js'
 
 class ObservableAppState extends EventEmitter {
-  page = ''
 
-  /** @type {import('./models/Value.js').Value[]} */
-  values = loadState('values', [Value])
-
-  // NOTE Used to load initial data
-  init() {
-
-  }
-
+  /**@type {import('./models/Example.js').Example[]} */
+  examples = []
 }
 
-export const AppState = new Proxy(new ObservableAppState(), {
-  get(target, prop) {
-    isValidProp(target, prop)
-    return target[prop]
-  },
-  set(target, prop, value) {
-    isValidProp(target, prop)
-    target[prop] = value
-    target.emit(prop, value)
-    return true
-  }
-})
+export const AppState = createObservableProxy(new ObservableAppState())
